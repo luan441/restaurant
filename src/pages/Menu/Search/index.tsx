0059554ec18f623524b3a62cd21572673b5d5816
luan441 <luanhenrique441@gmail.com>
2022-10-07 15:45:08 -0300

    :sparkles: feat: criacao da header, nav e buscador

---
+++ src/pages/Menu/Search/index.tsx
@@ -0,0 +1,20 @@
+import styles from './Seach.module.scss';
+import { CgSearch } from 'react-icons/cg';
+
+interface Props {
+  setSearch: React.Dispatch<React.SetStateAction<string>>
+  search: string
+}
+
+export default function Search({ search, setSearch }: Props) {
+  return (
+    <div className={styles.search}>
+      <input
+        type="text"
+        onChange={(event) => setSearch(event.target.value)}
+        value={search}
+      />
+      <CgSearch size={20} color='#4c4d5e' />
+    </div>
+  );
+}
