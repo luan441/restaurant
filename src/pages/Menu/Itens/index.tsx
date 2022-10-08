6270c2d7020467dd9beee372ca199038d369b189
luan441 <luanhenrique441@gmail.com>
2022-10-08 15:53:12 -0300

    :recycle: refactor: colocando o codigo em conformidade com as configuracoes do eslint

--- src/pages/Menu/Itens/index.tsx
+++ src/pages/Menu/Itens/index.tsx
@@ -24,17 +24,17 @@ export default function Itens({ search, filter, orderer }: Props) {
 
   const ordererBy = (newList: typeof itens) => {
     switch (orderer) {
-      case 'porcao':
-        return newList.sort((a, b) => a.size > b.size ? 1 : -1);
+    case 'porcao':
+      return newList.sort((a, b) => a.size > b.size ? 1 : -1);
       
-      case 'qtd_pessoas':
-        return newList.sort((a, b) => a.serving > b.serving ? 1 : -1);
+    case 'qtd_pessoas':
+      return newList.sort((a, b) => a.serving > b.serving ? 1 : -1);
       
-      case 'preco':
-        return newList.sort((a, b) => a.price > b.price ? 1 : -1);
+    case 'preco':
+      return newList.sort((a, b) => a.price > b.price ? 1 : -1);
       
-      default:
-        return newList;
+    default:
+      return newList;
     }
   };
 
--- src/pages/Menu/Orderer/index.tsx
+++ src/pages/Menu/Orderer/index.tsx
@@ -2,7 +2,7 @@ import styles from './Order.module.scss';
 import options from './options.json';
 import { useState } from 'react';
 import classNames from 'classnames';
-import { MdKeyboardArrowUp, MdKeyboardArrowDown } from 'react-icons/md'
+import { MdKeyboardArrowUp, MdKeyboardArrowDown } from 'react-icons/md';
 
 interface Props {
   orderer: string
@@ -11,7 +11,7 @@ interface Props {
 
 export default function Orderer({ orderer, setOrderer }: Props) {
   const [open, setOpen] = useState<boolean>(false);
-  const nameOrderer = orderer && options.find((option) => option.value === orderer)?.name
+  const nameOrderer = orderer && options.find((option) => option.value === orderer)?.name;
 
   return (
     <button 
@@ -29,9 +29,9 @@ export default function Orderer({ orderer, setOrderer }: Props) {
         <MdKeyboardArrowDown size={20} />
       )}
       <div className={classNames({
-          [styles.orderer__options]: true,
-          [styles['orderer__options--active']]: open,
-        })}
+        [styles.orderer__options]: true,
+        [styles['orderer__options--active']]: open,
+      })}
       >
         {options.map((option) => (
           <div 
