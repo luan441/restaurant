9d79784f514410a04e05be121288c13db42ba79f
luan441 <luanhenrique441@gmail.com>
2022-10-09 11:47:32 -0300

    :sparkles: feat: links no componete de navegacao

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,10 +1,35 @@
 import {ReactComponent as Logo} from 'assets/logo.svg';
 import styles from './Nav.module.scss';
+import { Link } from 'react-router-dom';
 
 export default function Nav() {
+  const routes = [
+    {
+      label: 'Início',
+      to: '/'
+    },
+    {
+      label: 'Menu',
+      to: '/menu'
+    },
+    {
+      label: 'Sobre',
+      to: '/about'
+    }
+  ];
+
   return (
     <nav className={styles.nav}>
       <Logo />
+      <ul className={styles.nav__list}>
+        {routes.map((route, index) => (
+          <li key={index} className={styles.nav__link}>
+            <Link to={route.to}>
+              {route.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </nav>
   );
 }
