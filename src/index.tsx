7f4779b28c9e2ef9f80fddf74af00cb15c69137d
luan441 <luanhenrique441@gmail.com>
2022-10-09 11:03:15 -0300

    :sparkles: feat: page Home

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import 'normalize.css';
 import './index.css';
 import Menu from './pages/Menu';
+import Home from 'pages/Home';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Menu />
+    <Home />
   </React.StrictMode>
 );
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,25 @@
+import menu from 'data/menu.json';
+import styles from './Home.module.scss';
+
+export default function Home() {
+  const recommendedPlates = [...menu].sort(() => 0.5 - Math.random()).splice(0,3);
+  return (
+    <section>
+      <h3 className={styles.title}>
+        Recomendações da cozinha
+      </h3>
+      <div className={styles.recommendations}>
+        {recommendedPlates.map((item) => (
+          <div key={item.id} className={styles.recommended}>
+            <div className={styles.recommended__image}>
+              <img src={item.photo} alt={item.title} />
+            </div>
+            <button className={styles.recommended__button}>
+              Ver mais
+            </button>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
