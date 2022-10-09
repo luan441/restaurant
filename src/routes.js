b26269eaeef1af0b274e0450fd5973412ceca7f0
luan441 <luanhenrique441@gmail.com>
2022-10-09 12:43:20 -0300

    :sparkles: feat: criacao da pagina sobre

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import Home from 'pages/Home';
 import Menu from 'pages/Menu';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import StandardPage from 'components/StandardPage';
+import About from 'pages/About';
 
 export default function AppRouter() {
   return (
@@ -13,6 +14,7 @@ export default function AppRouter() {
           <Route path='/' element={<StandardPage />}>
             <Route index element={<Home />} />
             <Route path='menu' element={<Menu />} />
+            <Route path='about' element={<About />} />
           </Route>
         </Routes>
       </Router>
