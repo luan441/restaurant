65d9b54dbcc3e892864416a12bfeed2088449d2b
luan441 <luanhenrique441@gmail.com>
2022-10-09 11:20:52 -0300

    :heavy_plus_sign: build: add react router dom

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'normalize.css';
 import './index.css';
-import Menu from './pages/Menu';
-import Home from 'pages/Home';
+import AppRouter from 'routes';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Home />
+    <AppRouter/>
   </React.StrictMode>
 );
