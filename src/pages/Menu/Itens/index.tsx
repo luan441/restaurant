f3bc55af31f2ede9c987f00e61f7f9c9b1558fdd
luan441 <luanhenrique441@gmail.com>
2022-10-09 11:02:57 -0300

    :truck: chore: movendo dados externos para a pasta data

--- src/pages/Menu/Itens/index.tsx
+++ src/pages/Menu/Itens/index.tsx
@@ -1,5 +1,5 @@
 import styles from './Itens.module.scss';
-import itens from './itens.json';
+import itens from 'data/menu.json';
 import Item from './Item';
 import { useEffect, useState } from 'react';
 
@@ -26,13 +26,13 @@ export default function Itens({ search, filter, orderer }: Props) {
     switch (orderer) {
     case 'porcao':
       return newList.sort((a, b) => a.size > b.size ? 1 : -1);
-      
+
     case 'qtd_pessoas':
       return newList.sort((a, b) => a.serving > b.serving ? 1 : -1);
-      
+
     case 'preco':
       return newList.sort((a, b) => a.price > b.price ? 1 : -1);
-      
+
     default:
       return newList;
     }
