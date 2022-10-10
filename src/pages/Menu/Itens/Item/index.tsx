3fedf9d21a8f2db516b247555197a9cc357b235c
luan441 <luanhenrique441@gmail.com>
2022-10-10 15:34:19 -0300

    :sparkles: feat: clique em prato do menu redireciona para pagina do prato

--- src/pages/Menu/Itens/Item/index.tsx
+++ src/pages/Menu/Itens/Item/index.tsx
@@ -1,10 +1,16 @@
 import styles from './Item.module.scss';
 import { Plate } from 'types/Plate';
 import PlateTags from 'components/PlateTags';
+import { useNavigate } from 'react-router-dom';
 
 export default function Item(props: Plate) {
+  const navigate = useNavigate();
+
   return (
-    <div className={styles.item}>
+    <div
+      className={styles.item}
+      onClick={() => navigate(`/plate/${props.id}`)}
+    >
       <div className={styles.item__image}>
         <img src={props.photo} alt={ props.title } />
       </div>
